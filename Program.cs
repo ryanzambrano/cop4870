1569d2b6aa6b4262acf40e1da7d90e80fe4e35c4
Ryan Zambrano <rez1237@gmail.com>
2025-01-08 14:27:32 -0500

    initial push

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create an instance of your Test class
+        Test testObject = new Test();
+        
+        // Call methods from your Test class
+        testObject.YourMethod();
+    }
+}
---
+++ test.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Test
+{
+    public void YourMethod()
+    {
+        Console.WriteLine("Method from Test class");
+    }
+}
